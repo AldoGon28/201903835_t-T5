74cbff10b8dbe03cb0953125782ebd33f95b2664
AldoGon28 <99936162+AldoGon28@users.noreply.github.com>
2022-02-27 23:05:31 -0600

    Add files via upload

---
+++ Module1.vb
@@ -0,0 +1,23 @@
+﻿Module Module1
+    Sub Main(args As String())
+
+        Dim a As Integer
+        Dim b As Integer
+        Dim c As Integer
+        b = 1
+        Console.WriteLine("Ingrese el Número a cálcular:")
+        a = Console.ReadLine()
+        For i = 1 To a
+            b = b * i
+        Next
+        Console.WriteLine("El Factorial del Número es: " + b.ToString)
+        For x = 0 To b
+            If (x Mod 4) Then
+                c = c + 1
+            End If
+        Next
+        Console.WriteLine("Hay" + c.ToString + " numeros multiplos de 4 en " + b.ToString)
+        Console.ReadKey()
+    End Sub
+
+End Module
